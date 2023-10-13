8d96be208c22074bf8298d8a27e584dc9b44e14f
iiitvamsi <bendalamkrishnavamsi@gmail.com>
2023-10-14 02:26:57 +0530

    bug fix image related issue

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,49 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../css/ProductDetails.css';
 import { useParams } from 'react-router-dom';
 import productsList from '../productsList';
 
-/**
- * @description "T-Shirt Product Images"
- */
-// import ts1 from "../images/tshirt_p1/ts_1.jpg";
-// import ts2 from "../images/tshirt_p1/ts_2.jpg";
-// import ts3 from "../images/tshirt_p1/ts_3.jpg";
-// import ts4 from "../images/tshirt_p1/ts_4.jpg";
-// import ts5 from "../images/tshirt_p1/ts_5.jpg";
 
 function ProductDetails() {
-    // let props = {
-    //     idProduct: 1,
-    //     name: "Polo Neck Half Sleeve Soild Casual Tshirt",
-    //     price: 183,
-    //     aboutProduct: [
-    //         "Care Instructions: Machine Wash",
-    //         "Fit Type: Regular Fit",
-    //         "Fabric: Cotton Blend; Premium Export Quality Branded T-shirt; Unique Collection to your wardrobe casuals a hit of effortless cool with this best-looking t shirt.",
-    //         "Sleeve Type: Half Sleeve; Collar Style: Polo Neck",
-    //         "Quality: All garments are subjected to the following tests Fabric dimensional stability test and quality inspection for colours and wash fastness."
-    //     ],
-    //     images: [
-    //         { idImage: 1, image: ts1 },
-    //         { idImage: 2, image: ts2 },
-    //         { idImage: 3, image: ts3 },
-    //         { idImage: 4, image: ts4 },
-    //         { idImage: 5, image: ts5 }
-    //     ]
-    // }
-
-    // use "useState()" for selected Image update in Image Div
-    
     const { productId } = useParams();
     const [productImageData, setProductImageData] = useState();
     let selProduct;
 
-    useEffect(() => {
-        selProduct = productsList.find((_list) => _list.idProduct === productId);
-        setProductImageData(selProduct.images[0].image);
-    });
-    
+    for (let i = 0; i < productsList.length; i++) {
+        if (productsList[i].id === productId) {
+            selProduct = productsList[i]
+        }
+    }
+
+    // useEffect(() => {
+    //     selProduct = productsList.find((_list) => _list.idProduct === productId);
+    //     console.log(selProduct)
+    //     setProductImageData(selProduct.images[0].image);
+    // });
+
     // This function is used to update the selected image data into useState function
     let updateSelProductImage = (selProductImageDetails) => {
         setProductImageData(selProductImageDetails.image);
@@ -60,7 +37,9 @@ function ProductDetails() {
                             {
                                 selProduct.images.map((pdtImage) => {
                                     return (
-                                        <div className="card shadow product_image_div" key={pdtImage.idImage} onClick={() => updateSelProductImage(pdtImage)}>
+                                        <div className="card shadow product_image_div" key={pdtImage.idImage}
+                                            onClick={() => updateSelProductImage(pdtImage)}
+                                        >
                                             <img className="product_img" src={pdtImage.image} alt={`Image_${pdtImage.idImage}`} />
                                         </div>
                                     )
@@ -73,8 +52,8 @@ function ProductDetails() {
                             <img className="sel-product-image" src={productImageData} alt="Product Image" />
                         </div>
                     </div>
-                    
-                    <br/>
+
+                    <br />
                     {/* ADD Cart button */}
                     <div className="d-flex justify-content-end">
                         <button type="button" className="btn shadow add-cart-btn">
@@ -114,7 +93,7 @@ function ProductDetails() {
                 </div>
             </div>
 
-            <br/>
+            <br />
         </div>
     );
 }
